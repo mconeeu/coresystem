30ec16d39b111a39004f9bf683fa78116429acd8
Rufus Maiwald <mail@rufusmaiwald.de>
2020-04-04 12:23:14 +0000

    fixed import bug

--- bukkit/src/main/java/eu/mcone/coresystem/bukkit/world/WorldManager.java
+++ bukkit/src/main/java/eu/mcone/coresystem/bukkit/world/WorldManager.java
@@ -32,6 +32,7 @@ import org.bukkit.entity.EntityType;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
