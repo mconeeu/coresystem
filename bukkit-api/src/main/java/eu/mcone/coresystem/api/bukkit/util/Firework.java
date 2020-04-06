d8f42979058cc3a49403596451cbc5021265230c
dominiklippl <dominik.lippl@hotmail.de>
2020-04-06 02:05:46 +0200

    Firework class bug fix.

--- bukkit-api/src/main/java/eu/mcone/coresystem/api/bukkit/util/Firework.java
+++ bukkit-api/src/main/java/eu/mcone/coresystem/api/bukkit/util/Firework.java
@@ -3,22 +3,13 @@ package eu.mcone.coresystem.api.bukkit.util;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
-import org.bukkit.inventory.meta.FireworkMeta;
-
-import java.util.List;
 
 public class Firework {
 
-    private org.bukkit.entity.Firework firework;
     private FireworkEffect.Builder fireworkEffect_builder;
-    private FireworkMeta fireworkMeta;
 
-    public Firework(Location location) {
-//        (org.bukkit.entity.Firework) new EntityFireworks(((CraftWorld) location.getWorld()).getHandle(), location.getX(), location.getY(), location.getZ(), null).getBukkitEntity();
-        this.firework = (org.bukkit.entity.Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
+    public Firework() {
         this.fireworkEffect_builder = FireworkEffect.builder();
-        this.fireworkMeta = this.firework.getFireworkMeta();
     }
 
     public Firework color(Color color) {
@@ -56,21 +47,10 @@ public class Firework {
         return this;
     }
 
-    public Firework power(int power) {
-        this.fireworkMeta.setPower(power);
-        return this;
-    }
-
-    public org.bukkit.entity.Firework build() {
-        this.firework.setFireworkMeta(this.fireworkMeta);
-        return firework;
-    }
-
-    public int getPower() {
-        return this.fireworkMeta.getPower();
-    }
-
-    public List<FireworkEffect> getEffects() {
-        return this.fireworkMeta.getEffects();
+    public void spawn(Location location, int power) {
+        org.bukkit.entity.Firework firework = location.getWorld().spawn(location, org.bukkit.entity.Firework.class);
+        firework.getFireworkMeta().setPower(power);
+        firework.getFireworkMeta().addEffect(fireworkEffect_builder.build());
+        firework.setFireworkMeta(firework.getFireworkMeta());
     }
 }
